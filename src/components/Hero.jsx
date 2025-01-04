859458dee37faee50d72ef6f3592fb7eb91c7238
Sparsh-dokania <sparshdokania2004@gmail.com>
2025-01-05 01:36:54 +0530

    Add Navbar, Footer, Contact, and Features components; update App structure and dependencies

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,8 +5,6 @@ import { TiLocationArrow } from "react-icons/ti";
 import { useEffect, useRef, useState } from "react";
 
 import Button from "./Button";
-// import VideoPreview from "./VideoPreview";
-
 gsap.registerPlugin(ScrollTrigger);
 
 const Hero = () => {
@@ -101,7 +99,7 @@ const Hero = () => {
             >
                 <div>
                     <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
-                        {/* <VideoPreview> */}
+                    
                         <div
                             onClick={handleMiniVdClick}
                             className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
@@ -116,7 +114,7 @@ const Hero = () => {
                                 onLoadedData={handleVideoLoad}
                             />
                         </div>
-                        {/* </VideoPreview> */}
+                    
                     </div>
 
                     <video
